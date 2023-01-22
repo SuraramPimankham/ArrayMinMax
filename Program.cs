27f10156942894d8763c32359aed504fd02e9704
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2023-01-23 00:14:39 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ArrayMinMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[3];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("-------data-------");
+
+            foreach (int data in num)
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("-------Max-------");
+            Console.WriteLine(num.Max());
+            Console.WriteLine("-------Min-------");
+            Console.WriteLine(num.Min());
+        }
+    }
+}
